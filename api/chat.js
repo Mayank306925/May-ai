1a6355e664cf7c06833e635bebe29323fe9dd028
Mayank306925 <mayankbhati146@gmail.com>
2026-07-17 13:13:53 +0530

    Add May AI backend API

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,47 @@
 export default async function handler(req, res) {
-  res.status(200).json({
-    reply: "May AI backend is working!"
-  });
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+
+    const { message } = req.body;
+
+    const response = await fetch(
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" +
+      process.env.GEMINI_API_KEY,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: message
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    const reply =
+      data.candidates?.[0]?.content?.parts?.[0]?.text ||
+      "Sorry, mujhe answer nahi mila.";
+
+    res.status(200).json({ reply });
+
+  } catch (err) {
+    res.status(500).json({
+      reply: "Backend Error"
+    });
+  }
+
 }
